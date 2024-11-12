1b6dda47cee3741a18f34f0a82a18c9fa4cdbb63
guofeihong <1826252461@qq.com>
2024-11-12 19:28:53 +0800

    会员中心-我的订单列表实现

--- src/apis/user.js
+++ src/apis/user.js
@@ -25,3 +25,20 @@ export const getLikeListAPI = ({ limit = 4 }) => {
     }
   })
 }
+
+/*
+params: {
+	orderState:0,
+  page:1,
+  pageSize:2
+}
+*/
+
+// 获取用户订单
+export const getUserOrder = (params) => {
+  return http({
+    url:'/member/order',
+    method:'GET',
+    params
+  })
+}
